85f6ef9bb4df27aa9731cf2fba2d608a333bb389
Angelo Carlotto <angelocarlotto@gmail.com>
2024-05-22 10:37:35 -0400

    refactored the svg icons into one file

--- index.php
+++ index.php
@@ -20,7 +20,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	} else {
 		$succesullAdded = $database->createTask($title, $employee_id_creation, $employee_id_assigned, $start_date, $end_date, $task_status_id, $description);
 
-		 header("HTTP/1.1 301 Moved Permanently"); header("Location: index.php?task_id=$succesullAdded"); exit();
+		header("HTTP/1.1 301 Moved Permanently");
+		header("Location: index.php?task_id=$succesullAdded");
+		exit();
 	}
 }
 
@@ -58,10 +60,10 @@ if (array_key_exists("task_id", $_GET)) {
 	<header>
 		<nav class="navbar navbar-dark bg-primary">
 			<div class="container-fluid">
-				<a class="navbar-brand" href="index.php"><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-controller" viewBox="0 0 16 16">
-  <path d="M11.5 6.027a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1m2.5-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1m-6.5-3h1v1h1v1h-1v1h-1v-1h-1v-1h1z"/>
-  <path d="M3.051 3.26a.5.5 0 0 1 .354-.613l1.932-.518a.5.5 0 0 1 .62.39c.655-.079 1.35-.117 2.043-.117.72 0 1.443.041 2.12.126a.5.5 0 0 1 .622-.399l1.932.518a.5.5 0 0 1 .306.729q.211.136.373.297c.408.408.78 1.05 1.095 1.772.32.733.599 1.591.805 2.466s.34 1.78.364 2.606c.024.816-.059 1.602-.328 2.21a1.42 1.42 0 0 1-1.445.83c-.636-.067-1.115-.394-1.513-.773-.245-.232-.496-.526-.739-.808-.126-.148-.25-.292-.368-.423-.728-.804-1.597-1.527-3.224-1.527s-2.496.723-3.224 1.527c-.119.131-.242.275-.368.423-.243.282-.494.575-.739.808-.398.38-.877.706-1.513.773a1.42 1.42 0 0 1-1.445-.83c-.27-.608-.352-1.395-.329-2.21.024-.826.16-1.73.365-2.606.206-.875.486-1.733.805-2.466.315-.722.687-1.364 1.094-1.772a2.3 2.3 0 0 1 .433-.335l-.028-.079zm2.036.412c-.877.185-1.469.443-1.733.708-.276.276-.587.783-.885 1.465a14 14 0 0 0-.748 2.295 12.4 12.4 0 0 0-.339 2.406c-.022.755.062 1.368.243 1.776a.42.42 0 0 0 .426.24c.327-.034.61-.199.929-.502.212-.202.4-.423.615-.674.133-.156.276-.323.44-.504C4.861 9.969 5.978 9.027 8 9.027s3.139.942 3.965 1.855c.164.181.307.348.44.504.214.251.403.472.615.674.318.303.601.468.929.503a.42.42 0 0 0 .426-.241c.18-.408.265-1.02.243-1.776a12.4 12.4 0 0 0-.339-2.406 14 14 0 0 0-.748-2.295c-.298-.682-.61-1.19-.885-1.465-.264-.265-.856-.523-1.733-.708-.85-.179-1.877-.27-2.913-.27s-2.063.091-2.913.27"/>
-</svg>Task Logger</a>
+				<a class="navbar-brand" href="index.php">
+					<svg width="64" height="64" fill="currentColor" class="bi bi-controller">
+						<use href="sprintes.svg#bi-controller"></use>
+					</svg> Task Logger</a>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
@@ -84,7 +86,7 @@ if (array_key_exists("task_id", $_GET)) {
 		<section class="form-row row justify-content-center">
 
 			<?php
-			if ($succesullAdded!=-1) {
+			if ($succesullAdded != -1) {
 			?>
 				<div class="form-group submit-message">
 
@@ -107,21 +109,24 @@ if (array_key_exists("task_id", $_GET)) {
 
 				<h2>Create/Update Task</h2>
 				<div class="form-group">
-					<label for="input1" class="col-sm-2 control-label"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-heading" viewBox="0 0 16 16">
-  <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
-  <path d="M3 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0-5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5z"/>
-</svg>Task Title</label>
+					<label for="input1" class="col-sm-2 control-label">
+						<svg width="16" height="16" fill="currentColor" class="bi bi-card-heading">
+							<use href="sprintes.svg#bi-card-heading"></use>
+						</svg>
+						Task Title</label>
 					<div class="col-sm-10">
 						<input required type="text" name="title" class="form-control" id="input1" value="<?php echo $taskToUpdate != null ? $taskToUpdate["title"] : "" ?>">
 					</div>
 				</div>
 				<div class="form-group">
-					<label for="input4" class="col-sm-2 control-label"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
-  <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-  <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"/>
-</svg>Creator</label>
+					<label for="input4" class="col-sm-2 control-label">
+						<svg width="16" height="16" fill="currentColor" class="bi bi-person-plus">
+							<use href="sprintes.svg#bi-person-plus"></use>
+						</svg>
+
+						Creator</label>
 					<div class="col-sm-10">
-						<select  name="employee_id_creation" class="form-select" required="required" >
+						<select name="employee_id_creation" class="form-select" required="required">
 							<option value="">Who is creating this task:</option>
 							<?php foreach ($employees as $row) { ?>
 								<option value="<?php echo $row['employee_id'] ?>" <?php echo $taskToUpdate != null && $row['employee_id'] == $taskToUpdate["employee_id_creation"] ? "selected" : "" ?>><?php echo $row['full_name'] ?></option>
@@ -130,12 +135,16 @@ if (array_key_exists("task_id", $_GET)) {
 					</div>
 				</div>
 				<div class="form-group">
-					<label for="input4" class="col-sm-2 control-label"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-gear" viewBox="0 0 16 16">
-  <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m.256 7a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1zm3.63-4.54c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382zM14 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"/>
-</svg>Doer</label>
+					<label for="input4" class="col-sm-2 control-label">
+
+						<svg width="16" height="16" fill="currentColor" class="bi bi-person-gear">
+							<use href="sprintes.svg#bi-person-gear"></use>
+						</svg>
+
+						Doer</label>
 					<div class="col-sm-10">
-						
-						<select name="employee_id_assigned" class="form-select" required="required" >
+
+						<select name="employee_id_assigned" class="form-select" required="required">
 							<option value="">Assigned this task to:</option>
 							<?php foreach ($employees as $row) { ?>
 								<option value="<?php echo $row['employee_id'] ?>" <?php echo $taskToUpdate != null && $row['employee_id'] == $taskToUpdate["employee_id_assigned"] ? "selected" : "" ?>><?php echo $row['full_name'] ?></option>
@@ -145,30 +154,40 @@ if (array_key_exists("task_id", $_GET)) {
 				</div>
 
 				<div class="form-group">
-					<label for="input2" class="col-sm-2 control-label"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar2" viewBox="0 0 16 16">
-  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1z"/>
-  <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5z"/>
-</svg>Start Date</label>
+					<label for="input2" class="col-sm-4 control-label">
+
+
+						<svg width="16" height="16" fill="currentColor" class="bi bi-calendar2">
+							<use href="sprintes.svg#bi-calendar2"></use>
+						</svg>
+
+						Start Date</label>
 					<div class="col-sm-10">
 						<input required type="datetime-local" name="start_date" class="form-control" id="input2" value="<?php echo $taskToUpdate != null ? $taskToUpdate["start_date"] : "" ?>">
 					</div>
 				</div>
 				<div class="form-group">
-					<label for="input3" class="col-sm-2 control-label"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar2-check" viewBox="0 0 16 16">
-  <path d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
-  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1z"/>
-  <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5z"/>
-</svg>End Date</label>
+					<label for="input3" class="col-sm-4 control-label">
+
+						<svg width="16" height="16" fill="currentColor" class="bi bi-calendar2-check">
+							<use href="sprintes.svg#bi-calendar2-check"></use>
+						</svg>
+
+						End Date</label>
 					<div class="col-sm-10">
 						<input required type="datetime-local" name="end_date" class="form-control" id="input3" value="<?php echo $taskToUpdate != null ? $taskToUpdate["end_date"] : "" ?>">
 					</div>
 				</div>
 
 				<div class="form-group">
-					<label for="input4" class="col-sm-2 control-label"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup-hot" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M.5 6a.5.5 0 0 0-.488.608l1.652 7.434A2.5 2.5 0 0 0 4.104 16h5.792a2.5 2.5 0 0 0 2.44-1.958l.131-.59a3 3 0 0 0 1.3-5.854l.221-.99A.5.5 0 0 0 13.5 6zM13 12.5a2 2 0 0 1-.316-.025l.867-3.898A2.001 2.001 0 0 1 13 12.5M2.64 13.825 1.123 7h11.754l-1.517 6.825A1.5 1.5 0 0 1 9.896 15H4.104a1.5 1.5 0 0 1-1.464-1.175"/>
-  <path d="m4.4.8-.003.004-.014.019a4 4 0 0 0-.204.31 2 2 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.6.6 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3 3 0 0 1-.202.388 5 5 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 3.6 4.2l.003-.004.014-.019a4 4 0 0 0 .204-.31 2 2 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.6.6 0 0 0-.09-.252A4 4 0 0 0 3.6 2.8l-.01-.012a5 5 0 0 1-.37-.543A1.53 1.53 0 0 1 3 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a6 6 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 4.4.8m3 0-.003.004-.014.019a4 4 0 0 0-.204.31 2 2 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.6.6 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3 3 0 0 1-.202.388 5 5 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 6.6 4.2l.003-.004.014-.019a4 4 0 0 0 .204-.31 2 2 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.6.6 0 0 0-.09-.252A4 4 0 0 0 6.6 2.8l-.01-.012a5 5 0 0 1-.37-.543A1.53 1.53 0 0 1 6 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a6 6 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 7.4.8m3 0-.003.004-.014.019a4 4 0 0 0-.204.31 2 2 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.6.6 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3 3 0 0 1-.202.388 5 5 0 0 1-.252.382l-.019.025-.005.008-.002.002A.5.5 0 0 1 9.6 4.2l.003-.004.014-.019a4 4 0 0 0 .204-.31 2 2 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.6.6 0 0 0-.09-.252A4 4 0 0 0 9.6 2.8l-.01-.012a5 5 0 0 1-.37-.543A1.53 1.53 0 0 1 9 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a6 6 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 10.4.8"/>
-</svg>Status</label>
+					<label for="input4" class="col-sm-2 control-label">
+
+						<svg width="16" height="16" fill="currentColor" class="bi bi-cup-hot">
+							<use href="sprintes.svg#bi-cup-hot"></use>
+						</svg>
+
+
+						Status</label>
 					<div class="col-sm-10">
 						<select name="task_status_id" class="form-select" required>
 							<option value="">Select Task status:</option>
@@ -180,10 +199,15 @@ if (array_key_exists("task_id", $_GET)) {
 				</div>
 
 				<div class="form-group">
-					<label for="input3" class="col-sm-2 control-label"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-ol" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5"/>
-  <path d="M1.713 11.865v-.474H2c.217 0 .363-.137.363-.317 0-.185-.158-.31-.361-.31-.223 0-.367.152-.373.31h-.59c.016-.467.373-.787.986-.787.588-.002.954.291.957.703a.595.595 0 0 1-.492.594v.033a.615.615 0 0 1 .569.631c.003.533-.502.8-1.051.8-.656 0-1-.37-1.008-.794h.582c.008.178.186.306.422.309.254 0 .424-.145.422-.35-.002-.195-.155-.348-.414-.348h-.3zm-.004-4.699h-.604v-.035c0-.408.295-.844.958-.844.583 0 .96.326.96.756 0 .389-.257.617-.476.848l-.537.572v.03h1.054V9H1.143v-.395l.957-.99c.138-.142.293-.304.293-.508 0-.18-.147-.32-.342-.32a.33.33 0 0 0-.342.338zM2.564 5h-.635V2.924h-.031l-.598.42v-.567l.629-.443h.635z"/>
-</svg>Description</label>
+					<label for="input3" class="col-sm-4 control-label">
+
+
+						<svg width="16" height="16" fill="currentColor" class="bi bi-list-ol">
+							<use href="sprintes.svg#bi-list-ol"></use>
+						</svg>
+
+
+						Description</label>
 					<div class="col-sm-10">
 						<textarea name="description" required class="form-control" id="input4"><?php echo $taskToUpdate != null ? $taskToUpdate["description"] : "" ?></textarea>
 					</div>
@@ -192,10 +216,16 @@ if (array_key_exists("task_id", $_GET)) {
 				if (array_key_exists("task_id", $_GET)) {
 				?>
 					<div class="form-group">
-						<label for="input3" class="col-sm-2 control-label"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
-  <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
-  <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0M7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0"/>
-</svg>Solution</label>
+						<label for="input3" class="col-sm-2 control-label">
+
+
+							<svg width="16" height="16" fill="currentColor" class="bi bi-card-checklist">
+								<use href="sprintes.svg#bi-card-checklist"></use>
+							</svg>
+
+
+
+							Solution</label>
 						<div class="col-sm-10">
 							<textarea name="solution" class="form-control" id="input4"><?php echo $taskToUpdate != null ? $taskToUpdate["solution"] : "" ?></textarea>
 						</div>
@@ -208,7 +238,7 @@ if (array_key_exists("task_id", $_GET)) {
 
 				<div class="form-group">
 
-					<input type="submit" class="btn btn-success col-md-2 col-md-offset-10" value="<?php echo array_key_exists("task_id", $_GET)?"Update":"Insert" ?>">
+					<input type="submit" class="btn btn-success col-md-2 col-md-offset-10" value="<?php echo array_key_exists("task_id", $_GET) ? "Update" : "Insert" ?>">
 				</div>
 			</form>
 
@@ -217,13 +247,13 @@ if (array_key_exists("task_id", $_GET)) {
 	<script type="module" defer>
 		let end_date = document.getElementsByName("end_date")[0];
 		let start_date = document.getElementsByName("start_date")[0];
-		end_date.addEventListener("change",(e)=>{
-			
+		end_date.addEventListener("change", (e) => {
+
 			start_date["max"] = e.target["value"];
 		})
 
-		start_date.addEventListener("change",(e)=>{
-			
+		start_date.addEventListener("change", (e) => {
+
 			end_date["min"] = e.target["value"];
 		})
 	</script>
